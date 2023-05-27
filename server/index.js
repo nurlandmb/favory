d45966f20e21741638f8620cd814e42a17bb515b
nurlandmb <nzt.dmb@gmail.com>
2023-05-27 20:27:07 +0600

    fix: removed cors

--- server/index.js
+++ server/index.js
@@ -13,8 +13,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
+    // credentials: true,
+    // origin: process.env.CLIENT_URL,
   })
 );
 app.use('/api', router);
